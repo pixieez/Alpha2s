cb2181392161c0ced42773f318f76d76501d21cf
pixieez <160623847+pixieez@users.noreply.github.com>
2024-03-17 00:20:57 +0700

    Update tools.cmd

--- tools.cmd
+++ tools.cmd
@@ -23,12 +23,13 @@ echo:         [1] AnyDesk
 echo:         [2] WinRAR
 echo:         ________________________________________________________________
 echo.
+echo:         [f] Create Alpha2 folder
 echo:         [0] Exit
 echo:     ________________________________________________________________________ 
 echo:                                   2024 ^| Alpha2
 echo.
-echo:     Enter a menu option in the Keyboard [1,2,0] :
-choice /C:123456780 /N
+echo:     Enter a menu option in the Keyboard [1,2,f,0] :
+choice /C:123456780f /N
 set "userChoice=%errorlevel%"
 
 if %userChoice%==1 goto tools1
@@ -36,6 +37,7 @@ if %userChoice%==2 goto tools2
 if %userChoice%==3 goto tools3
 if %userChoice%==4 goto tools4
 if %userChoice%==9 goto EndScript
+if %userChoice%==f goto Alpha2Folder
 
 :tools1
 @echo off
@@ -56,6 +58,20 @@ cd /d %temp%
 winrar-x64-700.exe /S
 timeout /t 4
 start "" "rarkey.rar"
+
+:Alpha2Folder
+echo:  please set your path (e.g. : [D:\..\] or etc.)
+set /p alpha2_dir="  path:  "
+@echo off
+cd /d %alpha2_dir%
+mkdir .Alpha2
+echo [.ShellClassInfo] > .Alpha2\desktop.ini
+curl -L -o ./.Alpha2/Alpha2.ico "https://raw.githubusercontent.com/pixieez/Alpha2s/main/Alpha2.ico"
+echo IconResource=%CD%\.Alpha2\Alpha2.ico,0 >> .Alpha2\desktop.ini
+
+attrib +s +h .Alpha2\desktop.ini
+attrib +s +h .Alpha2\Alpha2.ico
+attrib +r .Alpha2
 
 :EndScript
 echo Exiting script...
